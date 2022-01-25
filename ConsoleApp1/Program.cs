fe12903ec6a2926737296d0d380c9253b9811b04
Сергей Храмцов <vocmarh@mail.ru>
2022-01-25 11:50:34 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string str = Console.ReadLine();
+            bool b = true;
+            str = str.ToLower();
+            str = str.Replace(" ", string.Empty);
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                if (str[i] != str[str.Length - i - 1])
+                    b = false;
+            }
+
+            Console.WriteLine(b ? "является паллиндромом" : "не является паллиндромом");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings.Lesson_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            int theLongestWord = 0;
+            int number = 0;
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                if (stringArray[i].Length > theLongestWord)
+                {
+                    theLongestWord = stringArray[i].Length;
+                    number = i;
+                }
+            }
+
+            Console.WriteLine($"Самое длинное слово: {stringArray[number]}");
+            Console.ReadKey();
+        }
+    }
+}
